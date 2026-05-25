7bcbac57bd708ecd641fc4278e348ddaeea6ce0c
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-25 19:54:38 +0200

    Add project files.

---
+++ Tutorials/Hello world and text/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world_and_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
