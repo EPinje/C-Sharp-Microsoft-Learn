ea7e8563b8965fb114a7c904b0c3f148ee514b8d
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-25 19:56:58 +0200

    Hello world and text completed

--- Tutorials/Hello world and text/Program.cs
+++ Tutorials/Hello world and text/Program.cs
@@ -10,6 +10,66 @@ namespace Hello_world_and_text
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+
+            //string
+
+            string aFriend = "Bill";
+            Console.WriteLine("Hello " + aFriend);
+
+            aFriend = "Maira";
+            Console.WriteLine("Hello " + aFriend);
+
+            Console.WriteLine($"Hello {aFriend}");
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+            //Console.WriteLine({firstFriend.})
+
+            //Trim: TrimStart and TrimEnd
+
+            string greeting = "      Hello World!      ";
+            Console.WriteLine($"[{greeting}]");
+
+            string trimmedGreeting = greeting.TrimStart();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.TrimEnd();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+
+            //Search and replace
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            //upper and lowercase letters
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            //Contains
+
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            //StartsWith and EndsWith
+
+            Console.WriteLine(songLyrics.StartsWith("You"));
+            Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+            Console.WriteLine(songLyrics.EndsWith("hello"));
+            Console.WriteLine(songLyrics.EndsWith("goodbye"));
         }
     }
 }
